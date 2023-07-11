909bc5786218a2876ae578b30401fc2e6ac92d48
Vaibhav Avachat <vaibhav.a.avachat@outlook.com>
2023-07-11 03:00:39 -0400

    Added remove list duplicate

--- DataStructures/ListComprehension.py
+++ DataStructures/ListComprehension.py
@@ -13,7 +13,18 @@ class ListComprehensions:
 
     def list_appending_using_lambda_expression(self, rangeVal): 
         return list(map(lambda x:x**2,range(rangeVal)))
+    
+    def list_remove_duplicates(self, itemList: list): 
+        if itemList: 
+            itemList.sort()
+            last = itemList[-1]
+            for i in range(len(itemList)-2, -1, -1): 
+                if last == itemList[i]: 
+                    del itemList[i]
+                else: 
+                    last = itemList[i]
 
+        return itemList
 
 lcInstance =  ListComprehensions()
 
@@ -33,3 +44,7 @@ print("Initial Squares list: ",newSquaresTwo)
 
 newSquaresTwo = lcInstance.list_appending_expression(10)
 print("After appending list using for loop: ",newSquares)
+
+duplicatedItemsInList = [1,2,1,4,2,4,3,5,6,7,3,4,8,9,8,5,10]
+print("Duplicated Items list: ", duplicatedItemsInList)
+print("After removing duplicates from given list: ", lcInstance.list_remove_duplicates(duplicatedItemsInList))
