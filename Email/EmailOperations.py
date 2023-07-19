16b32683a9f2bec64db20eaf88b8cc02f2cb41f7
Vaibhav Avachat <vaibhav.a.avachat@outlook.com>
2023-07-19 07:20:37 -0400

    reading missing file checked in July 14th.

---
+++ Email/EmailOperations.py
@@ -0,0 +1,53 @@
+import sys 
+import smtplib
+
+# Sending an email from python script
+class EmailOperations: 
+    host: str = ""
+    port: int = 0
+
+    # Initializing host and port
+    def __init__(self, hostAddress:str , portNumber: int) -> None:
+        self.host = hostAddress
+        self.port = portNumber
+
+    # Send email - causing connection error and not sending message as expected.
+    def send_email(self) -> bool: 
+        try:
+            fromaddr = input("From: ")
+            toaddrs = input("To: ").split(",")
+            print("Enter message end with ^D: ")
+            msg = ''
+            lineCount =0
+            while True: 
+                line = sys.stdin.readline()
+                lineCount += 1
+                msg += line
+                if lineCount == 2: 
+                    break
+                
+            # The actual mail send
+            server = smtplib.SMTP(self.host,self.port)
+            server.sendmail(fromaddr, toaddrs, msg)
+            server.quit()
+            return True
+        except Exception as ex: 
+            print(ex)
+            return False
+
+    # Receive email
+    def receive_email(self) -> None: 
+        try: 
+            server = smtplib.SMTP(self.host)
+            reply = server.getreply()
+            print(reply)
+        except Exception as ex: 
+            print(ex)
+        
+if __name__ == '__main__': 
+    try: 
+        eoInstance = EmailOperations('localhost',25)
+        if eoInstance.send_email() == True : 
+            eoInstance.receive_email()
+    except Exception as ex:
+        print(ex)
