29a3299fa2b35e465af22229700a4557e414f4cf
Vaibhav Avachat <vaibhav.a.avachat@outlook.com>
2023-08-16 09:33:37 -0400

    added multiple approaches for api call - in progress for lint

---
+++ ApiSocketCalls/api_calls.py
@@ -0,0 +1,58 @@
+""" Module for Fast Api operations. """
+# Using serve approach from online
+import asyncio
+from typing import Union
+from fastapi import FastAPI
+from pydantic import BaseModel #type : ignore
+import uvicorn
+
+
+class Item(BaseModel):
+    """Item Model class """
+    name: str
+    data:list[str]
+
+# app = FastAPI()
+
+# @app.get("/")
+class ApiController:
+    """Api Controller class """
+    app : FastAPI
+
+    def __init__(self) -> None:
+        self.app = FastAPI()
+
+    async def serve(self):
+        """Fast API Serve """
+        app: FastAPI = self.app
+
+        @app.get("/")
+        async def read_root()-> dict[str, str | int]: #type : ignore
+            """Read root """
+            return  {"item_name": "Default item", "item_id": 1}
+
+        @app.get("/items/{item_id}")
+        async def read_item(item_id: int,
+                            input_qry: Union[str, None]=None) -> dict[str, str | int]:
+            """Read Items """
+            return {"item_id": item_id, "q": input_qry} # type: ignore
+
+        @app.post("/items/{item_id}")
+        async def write_item(item_id: int, item: Item) -> dict[str, Item | int]:
+            """Write item"""
+            return {"item": item, "item_id": item_id}
+
+        @app.put("/items/{item_id}")
+        async def update_item(item_id: int, item: Item) -> dict[str, str | list[str] | int]:
+            """Update item"""
+            return {"item_name": item.name, "item_data": item.data, "item_id": item_id}
+
+
+        uvicorn_config =  uvicorn.Config(app, host='localhost',port=8080)
+        uvicorn_server =  uvicorn.Server(uvicorn_config)
+        await uvicorn_server.serve()
+
+# application would run at localhost using port 8080
+if __name__ == "__main__":
+    controller = ApiController()
+    asyncio.run(controller.serve())
