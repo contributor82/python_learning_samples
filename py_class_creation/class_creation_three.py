1ca2e9a4ce666f6092e179255f676d9f613ba245
Vaibhav Avachat <vaibhav.a.avachat@outlook.com>
2023-09-06 05:16:32 -0400

    commented unsupported static method with self causing pylint warning

--- py_class_creation/class_creation_three.py
+++ py_class_creation/class_creation_three.py
@@ -1,7 +1,5 @@
 """Module for Static class """
 
-from typing import Self
-
 class StaticClass:
     """Static Class """
     int_mem: int
@@ -14,14 +12,14 @@ class StaticClass:
         """static display method """
         print('Hello World! This is a static method. ')
 
-    @staticmethod
-    def display_members(Self) -> None: #type: ignore # Static members doesn't permit.
-        """static display members method """
-        print("Static Class Members : int_mem : ", Self.int_mem) #type: ignore
+    # @staticmethod
+    # def display_members(self) -> None: #type: ignore
+    #     """static display members method """
+    #     print("Static Class Members : int_mem : ", self.int_mem) #type: ignore
 
 
     @staticmethod
-    def display_members(class_int_mem : int ) -> None:
+    def display_class_members(class_int_mem : int ) -> None:
         """static display members method """
         print("Static Class Members : int_mem : ", class_int_mem)
 
@@ -33,17 +31,18 @@ if __name__ == '__main__':
     StaticClass.int_mem = 10
     StaticClass.display()
     # not recommended.
-    StaticClass.display_members(StaticClass) #type: ignore
+    # StaticClass.display_members(StaticClass) #type: ignore
 
 
-    print('Creating static class instance as a separate instance variable \n and then calling its respective methods. ')
+    print('Creating static class instance as a separate instance variable' +
+          '\n and then calling its respective methods. ')
     static_cls_instance = StaticClass()
     static_cls_instance.display()
     # not recommended.
-    static_cls_instance.display_members(StaticClass) #type: ignore
-    static_cls_instance.display_members(StaticClass.int_mem)
+    # static_cls_instance.display_members(StaticClass) #type: ignore
+    static_cls_instance.display_class_members(StaticClass.int_mem)
 
     StaticClass().display()
-     # not recommended.
-    StaticClass().display_members(StaticClass) #type: ignore
-    StaticClass().display_members(StaticClass.int_mem)
+    # not recommended.
+    # StaticClass().display_members(StaticClass) #type: ignore
+    StaticClass().display_class_members(StaticClass.int_mem)
