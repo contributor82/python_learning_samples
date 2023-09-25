b8eb3ac925529090b194a707bad7408533e9e2bf
Git Contributor <74165300+my382@users.noreply.github.com>
2023-09-25 00:00:04 -0400

    Added few more data type conversion test cases.

--- py_converters/test_converters.py
+++ py_converters/test_converters.py
@@ -4,7 +4,7 @@ from decimal import Decimal
 import unittest
 from enum import Enum, IntEnum
 from py_converter import PyTypeConverters
-
+from typing import Type
 class Color(Enum):
     """Color enumeration class """
     RED = 0
@@ -30,8 +30,8 @@ class TestConverters(unittest.TestCase):
         result = self.converters.decimal_tostr(decimal_val)
         self.assertEqual('10',result)
 
-    def test_enum_tostr(self, enum_int_val: IntEnum = 2) -> None: # type: ignore
-        """test float tostr"""
+    def test_int_enum_tostr(self, enum_int_val: IntEnum = 2) -> None: # type: ignore
+        """test int enum tostr"""
         result = self.converters.int_enum_tostr(enum_int_val)
         self.assertEqual('2',result)
 
@@ -41,7 +41,7 @@ class TestConverters(unittest.TestCase):
         self.assertEqual(str(date.today()),result)
 
     def test_time_tostr(self, time_val : time = time(10,10,12,10)) -> None:
-        """test date to string sconversion """
+        """test time to string conversion """
         try:
             test_result: str | AssertionError
             result: str = self.converters.time_tostr(time_val)
@@ -52,6 +52,30 @@ class TestConverters(unittest.TestCase):
             test_result= assert_error
             self.assertTrue(test_result, AssertionError)
 
+    def test_str_toint(self, int_val : str = '10')-> None:
+        """test string to int conversion """
+        result: int = self.converters.str_toint(int_val)
+        self.assertEqual(10, result)
+
+    def test_str_tofloat(self, float_val: str = '10.5')->None:
+        """test string to float conversion """
+        result: float = self.converters.str_tofloat(float_val)
+        self.assertEqual(10.5,result)
+
+    def test_str_todecimal(self, decimal_val: str = '10.0')->None:
+        """test string to decimal conversion """
+        result: Decimal = self.converters.str_todecimal(decimal_val)
+        self.assertEqual(10.0,result)
+
+    def test_str_toenum(self, enum_type: Enum = Color, enum_val: str = "RED") -> None:
+        """test string to enum conversion """
+        result: Enum = self.converters.str_toenum(enum_type, enum_val)
+        self.assertEqual(Color.RED,result)
+
+    def test_str_todate(self, date_val: str = '2023-09-24')->None:
+        """test string to date conversion """
+        result: date = self.converters.str_todate(date_val)
+        self.assertEqual(date(2023,9,24),result)
 
 if __name__ == '__main__':
     unittest.main()
