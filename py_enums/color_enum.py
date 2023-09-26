0f76016523e3fcb809f8a99efd607d4bab8982ef
Git Contributor <74165300+my382@users.noreply.github.com>
2023-09-26 08:34:57 -0400

    Removed enum block causing pylint import errors for the time being.

--- py_enums/color_enum.py
+++
@@ -1,7 +0,0 @@
-"""Module for Color enumeration"""
-from enum import Enum
-class Color(Enum):
-    """Color enumeration class """
-    RED = 0
-    GREEN = 1
-    BLUE = 2
--- py_enums/vehicle_type_enum.py
+++
@@ -1,9 +0,0 @@
-"""Module for Vehicle type enum """
-
-from enum import Enum, _simple_enum, auto #type: ignore
-
-@_simple_enum(Enum) #type: ignore
-class VehicleTypes:
-    Sedan = auto()
-    Suv = auto()
-    HatchBack = auto()
